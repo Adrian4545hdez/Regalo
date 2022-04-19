d1a3af7e361d9afd05e643f78aa7498965d7b7c7
Adrian4545hdez <93615580+Adrian4545hdez@users.noreply.github.com>
2022-04-18 19:29:43 -0500

    Add files via upload

---
+++ Regalo/HTML/Gloria.js
@@ -0,0 +1,205 @@
+window.onload = Iniciar;
+function Iniciar() {
+    document.querySelector('#b1').addEventListener('click', proceso1)
+    document.querySelector('#b2').addEventListener('click', proceso2)
+    document.querySelector('#b3').addEventListener('click', proceso3)
+    document.querySelector('#b4').addEventListener('click', proceso4)
+    document.querySelector('#b5').addEventListener('click', proceso5)
+    document.querySelector('#b6').addEventListener('click', proceso6)
+    document.querySelector('#b7').addEventListener('click', proceso7)
+    document.querySelector('#b8').addEventListener('click', proceso8)
+    document.querySelector('#b9').addEventListener('click', proceso9)
+    document.querySelector('#b10').addEventListener('click', proceso10)
+    document.querySelector('#b11').addEventListener('click', proceso11)
+    document.querySelector('#b12').addEventListener('click', proceso12)
+    document.querySelector('#b13').addEventListener('click', proceso13)
+    document.querySelector('#b14').addEventListener('click', proceso14)
+    document.querySelector('#b15').addEventListener('click', proceso15)
+    document.querySelector('#b16').addEventListener('click', proceso16)
+    document.querySelector('#b17').addEventListener('click', proceso17)
+    document.querySelector('#b18').addEventListener('click', proceso18)
+    document.querySelector('#b19').addEventListener('click', proceso19)
+    document.querySelector('#b20').addEventListener('click', proceso20)
+    document.querySelector('#b21').addEventListener('click', proceso21)
+    document.querySelector('#b22').addEventListener('click', proceso22)
+    document.querySelector('#b23').addEventListener('click', proceso23)
+    document.querySelector('#b24').addEventListener('click', proceso24)
+    document.querySelector('#b25').addEventListener('click', proceso25)
+    document.querySelector('#b26').addEventListener('click', proceso26)
+    document.querySelector('#b27').addEventListener('click', proceso27)
+    document.querySelector('#b28').addEventListener('click', proceso28)
+    document.querySelector('#b29').addEventListener('click', proceso29)
+    document.querySelector('#b30').addEventListener('click', proceso30)
+    document.querySelector('#b31').addEventListener('click', proceso31)
+    document.querySelector('#b32').addEventListener('click', proceso32)
+    document.querySelector('#b33').addEventListener('click', proceso33)
+    document.querySelector('#b34').addEventListener('click', proceso34)
+    document.querySelector('#b35').addEventListener('click', proceso35)
+    document.querySelector('#b36').addEventListener('click', proceso36)
+    document.querySelector('#b37').addEventListener('click', proceso37)
+    document.querySelector('#b38').addEventListener('click', proceso38)
+    document.querySelector('#b39').addEventListener('click', proceso39)
+    document.querySelector('#b40').addEventListener('click', proceso40)
+}
+
+function proceso1() {
+    document.getElementById("c1").style.display = 'none';
+    document.getElementById("c2").style.display = 'block';
+}
+function proceso2() {
+    document.getElementById("c2").style.display = 'none';
+    document.getElementById("c3").style.display = 'block';
+}
+function proceso3() {
+    document.getElementById("c3").style.display = 'none';
+    document.getElementById("c4").style.display = 'block';
+}
+function proceso4() {
+    document.getElementById("c4").style.display = 'none';
+    document.getElementById("c5").style.display = 'block';
+}
+function proceso5() {
+    document.getElementById("c5").style.display = 'none';
+    document.getElementById("c6").style.display = 'block';
+}
+function proceso6() {
+    document.getElementById("c6").style.display = 'none';
+    document.getElementById("c7").style.display = 'block';
+}
+function proceso7() {
+    document.getElementById("c7").style.display = 'none';
+    document.getElementById("c8").style.display = 'block';
+}
+function proceso8() {
+    document.getElementById("c8").style.display = 'none';
+    document.getElementById("c9").style.display = 'block';
+}
+function proceso9() {
+    document.getElementById("c9").style.display = 'none';
+    document.getElementById("c10").style.display = 'block';
+}
+function proceso10() {
+    document.getElementById("c10").style.display = 'none';
+    document.getElementById("c11").style.display = 'block';
+}
+function proceso11() {
+    document.getElementById("c11").style.display = 'none';
+    document.getElementById("c12").style.display = 'block';
+}
+function proceso12() {
+    document.getElementById("c12").style.display = 'none';
+    document.getElementById("c13").style.display = 'block';
+}
+function proceso13() {
+    document.getElementById("c13").style.display = 'none';
+    document.getElementById("c14").style.display = 'block';
+}
+function proceso14() {
+    document.getElementById("c14").style.display = 'none';
+    document.getElementById("c15").style.display = 'block';
+}
+function proceso15() {
+    document.getElementById("c15").style.display = 'none';
+    document.getElementById("c16").style.display = 'block';
+}
+function proceso16() {
+    document.getElementById("c16").style.display = 'none';
+    document.getElementById("c17").style.display = 'block';
+}
+function proceso17() {
+    document.getElementById("c17").style.display = 'none';
+    document.getElementById("c18").style.display = 'block';
+}
+function proceso18() {
+    document.getElementById("c18").style.display = 'none';
+    document.getElementById("c19").style.display = 'block';
+}
+function proceso19() {
+    document.getElementById("c19").style.display = 'none';
+    document.getElementById("c20").style.display = 'block';
+}
+function proceso20() {
+    document.getElementById("c20").style.display = 'none';
+    document.getElementById("c21").style.display = 'block';
+}
+function proceso21() {
+    document.getElementById("c21").style.display = 'none';
+    document.getElementById("c22").style.display = 'block';
+}
+function proceso22() {
+    document.getElementById("c22").style.display = 'none';
+    document.getElementById("c23").style.display = 'block';
+}
+function proceso23() {
+    document.getElementById("c23").style.display = 'none';
+    document.getElementById("c24").style.display = 'block';
+}
+function proceso24() {
+    document.getElementById("c24").style.display = 'none';
+    document.getElementById("c25").style.display = 'block';
+}
+function proceso25() {
+    document.getElementById("c25").style.display = 'none';
+    document.getElementById("c26").style.display = 'block';
+}
+function proceso26() {
+    document.getElementById("c26").style.display = 'none';
+    document.getElementById("c27").style.display = 'block';
+}
+function proceso27() {
+    document.getElementById("c27").style.display = 'none';
+    document.getElementById("c28").style.display = 'block';
+}
+function proceso28() {
+    document.getElementById("c28").style.display = 'none';
+    document.getElementById("c29").style.display = 'block';
+}
+function proceso29() {
+    document.getElementById("c29").style.display = 'none';
+    document.getElementById("c30").style.display = 'block';
+}
+function proceso30() {
+    document.getElementById("c30").style.display = 'none';
+    document.getElementById("c31").style.display = 'block';
+}
+function proceso31() {
+    document.getElementById("c31").style.display = 'none';
+    document.getElementById("c32").style.display = 'block';
+}
+function proceso32() {
+    document.getElementById("c32").style.display = 'none';
+    document.getElementById("c33").style.display = 'block';
+}
+function proceso33() {
+    document.getElementById("c33").style.display = 'none';
+    document.getElementById("c34").style.display = 'block';
+}
+function proceso34() {
+    document.getElementById("c34").style.display = 'none';
+    document.getElementById("c35").style.display = 'block';
+}
+function proceso35() {
+    document.getElementById("c35").style.display = 'none';
+    document.getElementById("c36").style.display = 'block';
+}
+function proceso36() {
+    document.getElementById("c36").style.display = 'none';
+    document.getElementById("c37").style.display = 'block';
+}
+function proceso37() {
+    document.getElementById("c37").style.display = 'none';
+    document.getElementById("c38").style.display = 'block';
+}
+function proceso38() {
+    document.getElementById("c38").style.display = 'none';
+    document.getElementById("c39").style.display = 'block';
+}
+function proceso39() {
+    document.getElementById("c39").style.display = 'none';
+    document.getElementById("c40").style.display = 'block';
+}
+function proceso40() {
+    document.getElementById("c40").style.display = 'none';
+    document.getElementById("c41").style.display = 'block';
+}
+
